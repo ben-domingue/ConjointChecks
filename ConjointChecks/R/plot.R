a9839fc54880c4187c961f0c0cb9c74e867acb9e
Your Name <ben.domingue@gmail.com>
2012-02-29 05:36:44 -0700

    preparing things prior to adding data.

--- ConjointChecks/R/plot.R
+++ ConjointChecks/R/plot.R
@@ -1,4 +1,12 @@
-plot.checks<-function(x, ...) {
-  matplot(x@tab,xlab="Increasing Sum Scores",ylab="% Violations",type="l",lty=1,col="black")
+plot.checks<-function(x, items, item.labels=TRUE, ...) {
+  matplot(x@tab[,items],xlab="",xaxt="n",ylab="Proportion Violations",type="l",lty=1:length(items),col="black",...)
+  mtext(side=1,line=1,paste("Increasing Ability"))
+  if (item.labels) {
+    if (length(items)==1) mtext(side=1,line=2,paste("Item",items)) else {
+      apply(x@tab,2,which.max)->maxes.x
+      apply(x@tab,2,max,na.rm=TRUE)->maxes.y
+      for (i in 1:length(items)) text(maxes.x[items],maxes.y[items],items,pos=3)
+    }
+  }
 }
 
