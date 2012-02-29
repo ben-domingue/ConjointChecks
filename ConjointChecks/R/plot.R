9424dd99be029f4e85f08678a20229fc7da3b757
Your Name <ben.domingue@gmail.com>
2012-02-29 08:15:51 -0700

    handling dot-dot-dot args.

--- ConjointChecks/R/plot.R
+++ ConjointChecks/R/plot.R
@@ -1,7 +1,23 @@
 plot.checks<-function(x, items=NULL, item.labels=TRUE, ...) {
+  #graphical args
+  modify.args<-function(...,items) {
+    list(...)->args
+    if (is.null(args$ylim)) args$ylim<-c(0,1)
+    if (is.null(args$xlab)) args$xlab<-""
+    if (is.null(args$ylab)) args$ylab<-"Proportion Violations"
+    if (is.null(args$type)) args$type<-"l"
+    if (is.null(args$lty)) args$lty<-1:length(items)
+    if (is.null(args$col)) args$col<-"black"
+    if (is.null(args$xaxt)) args$xaxt<-"n"
+    args
+  }
+  #
   if (is.null(items)) items <- 1:ncol(x@tab)
   #
-  matplot(x@tab[,items],xlab="",xaxt="n",ylab="Proportion Violations",type="l",lty=1:length(items),col="black",...)
+  modify.args(...,items=items)->args
+  x@tab[,items]->dat
+  c(list(y=dat),args)->list.of.args
+  do.call("matplot",list.of.args)
   mtext(side=1,line=1,paste("Increasing Ability"))
   if (item.labels) {
     if (length(items)==1) mtext(side=1,line=2,paste("Item",items)) else {
@@ -11,6 +27,3 @@ plot.checks<-function(x, items=NULL, item.labels=TRUE, ...) {
     }
   }
 }
-
-
-
