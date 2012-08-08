e3f054b5f282bc5c85fa2763c599b0d4a2bd96dc
Ben Domingue <ben.domingue@gmail.com>
2012-08-08 08:02:20 -0600

    removing files

--- ConjointChecks/R/checks-class.R
+++
@@ -1,3 +0,0 @@
-setClassUnion("list.null", c("list","NULL"))
-setClass("checks", representation(N="matrix", n="matrix", Checks="list.null",tab="matrix",means="list",check.counts="matrix"))
-
--- ConjointChecks/R/summary.R
+++
@@ -1,3 +0,0 @@
-summary.checks<-function(object, ...) {
-  list(Means=object@means,items=colMeans(object@tab,na.rm=TRUE))
-}
